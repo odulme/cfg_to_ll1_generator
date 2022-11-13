deaa19d7cb96a740ef91987e0b7f5f0cf1e82cdb
ou <670323039@qq.com>
2022-11-14 00:51:25 +0800

    fix mil bug

--- lib/sets.mli
+++ lib/sets.mli
@@ -9,6 +9,6 @@ val minus : 'a list -> 'a list -> 'a list
 val union : 'a list -> 'a list -> 'a list
 val intersection : 'a list -> 'a list -> 'a list
 val product : 'a list -> 'b list -> ('a * 'b) list
-val cat : 'a -> 'b list -> ('a * 'b) list
+val cat : 'a list -> 'a list list -> 'a list list
+val illness_cat : 'a -> 'b list -> ('a * 'b) list
 val elem_find : 'a list -> bool
-val illness_cat : 'a -> 'b list -> ('a*'b) list
